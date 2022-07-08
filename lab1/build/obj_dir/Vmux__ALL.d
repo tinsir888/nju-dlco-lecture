b11fa2d9494d1897eb39444688da6054b0afaa18
tinsir888 <912658865@qq.com>
2022-07-08 12:20:33 +0800

    Delete lab1/build directory

--- lab1/build/obj_dir/Vmux__ALL.d
+++
@@ -1,6 +0,0 @@
-Vmux__ALL.o: Vmux__ALL.cpp Vmux.cpp Vmux.h \
- /usr/local/share/verilator/include/verilated_heavy.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h Vmux__Syms.h \
- Vmux___024root.h Vmux___024root.cpp Vmux___024root__Slow.cpp \
- Vmux__Syms.cpp
