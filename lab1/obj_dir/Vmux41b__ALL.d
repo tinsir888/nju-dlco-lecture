0e0118473e9a545a99a9eb319668355944b76102
tinsir888 <912658865@qq.com>
2022-07-08 12:20:45 +0800

    Delete lab1/obj_dir directory

--- lab1/obj_dir/Vmux41b__ALL.d
+++
@@ -1,9 +0,0 @@
-Vmux41b__ALL.o: Vmux41b__ALL.cpp Vmux41b.cpp Vmux41b.h \
- /usr/local/share/verilator/include/verilated_heavy.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h Vmux41b__Syms.h \
- Vmux41b___024root.h /usr/local/share/verilator/include/verilated_vcd_c.h \
- /usr/local/share/verilator/include/verilated_trace.h \
- /usr/local/share/verilator/include/verilated_trace_defs.h \
- Vmux41b___024root.cpp Vmux41b__Trace.cpp Vmux41b___024root__Slow.cpp \
- Vmux41b__Syms.cpp Vmux41b__Trace__Slow.cpp
